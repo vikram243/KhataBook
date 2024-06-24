f5c52856b9cb02a8dd6d1291e6d9817e0c26fa06
vikram243 <vikrammishra1042@gmail.com>
2024-06-25 00:16:06 +0530

    Project Done

--- models/users-validation.js
+++ models/users-validation.js
@@ -21,30 +21,4 @@ const userJoiSchema = Joi.object({
    .required(),
 });
 
-
-const hisaabJoiSchema = Joi.object({
-    title: Joi.string()
-     .trim()
-     .alphanum()
-     .min(3)
-     .max(100)
-     .required(),
-  
-    description: Joi.string()
-     .trim()
-     .required(),
-  
-    encrypted: Joi.boolean()
-     .default(false),
-  
-    shareable: Joi.boolean()
-     .default(false),
-  
-    passcode: Joi.string()
-     .default(''),
-  
-    editpermissions: Joi.boolean()
-     .default(false),
-  });
-  
-module.exports = {userJoiSchema,hisaabJoiSchema};
+module.exports = userJoiSchema;
